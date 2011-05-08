2d2e7482d05bfa705c35bc03cc3741363ba788a4
shazz <shazz.ng@gmail.com>
2011-05-08 11:39:24 +0200

    first implementation

--- zpuino/experiments/logsdk/src/ym2149/ym2149.c
+++ zpuino/experiments/logsdk/src/ym2149/ym2149.c
@@ -2,35 +2,40 @@
 
 void setDeviceSlot(unsigned int slot)
 {
-	
+	device_slot = slot;
 }
 
 void setChannelFrequency(ym2149_channel_t channel, uint16_t frequency)
 {
-	
+	YM2149_REG( (channel << 1) ) = (uint8_t)frequency;
+	YM2149_REG( ((channel << 1) + 1) ) = ((uint8_t) (frequency >> 8)) & 0x0F;
 }
 
 void setChannelVolume(ym2149_channel_t channel, uint8_t volume)
 {
-	
+	YM2149_REG( (YM2149_REG_LEVELBASE + channel) ) = volume;
 }
 
 void setNoiseFrequency(uint8_t frequency)
 {
-	
+	YM2149_REG(YM2149_REG_NOISE) = frequency;	
 }
 
 void setChannelMixer(ym2149_channel_t channel, ym2149_mixerType_t type)
 {
-	
+	if(type == YM2149_NOISE) 
+		YM2149_REG(YM2149_REG_MIXER) = channel << 3;
+	else 
+		YM2149_REG(YM2149_REG_MIXER) = channel;
 }
 
 void setEnvelopeFrequency(uint16_t frequency)
 {
-	
+	YM2149_REG(YM2149_REG_ENVELOPE_FINE) = (uint8_t)frequency;
+	YM2149_REG(YM2149_REG_ENVELOPE_ROUGH) = ((uint8_t) (frequency >> 8)) & 0x0F;	
 }
 
 void setEnvelopeShape(ym2149_envelopeShape_t envelope)
 {
-	
+	YM2149_REG(YM2149_REG_ENVELOPE_SHAPE) = envelope;
 }
--- zpuino/experiments/logsdk/src/ym2149/ym2149.h
+++ zpuino/experiments/logsdk/src/ym2149/ym2149.h
@@ -2,16 +2,25 @@
 #define __YM2149_H__
 
 #include <zpuino-types.h>
+#include <register.h>
 
+#define YM2149_BASE IO_SLOT(device_slot)
+#define YM2149_REG(x) REGISTER(YM2149_BASE,x)
 
-#define YM2149_BASE(s) IO_SLOT(s)
-#define YM2149_REG(c,x) REGISTER(c,x)
+#define YM2149_REG_LEVELBASE 		0x08
+#define YM2149_REG_ENVELOPE_FINE 	0x0B
+#define YM2149_REG_ENVELOPE_ROUGH 	0x0C
+#define YM2149_REG_ENVELOPE_SHAPE 	0x0D
+#define YM2149_REG_MIXER 			0x07
+#define YM2149_REG_NOISE 			0x06
 
 
 typedef enum ym2149_channel {YM2149_CH_A,YM2149_CH_B,YM2149_CH_C} ym2149_channel_t;
 typedef enum ym2149_mixerType {YM2149_NOISE, YM2149_TONE} ym2149_mixerType_t;
 typedef enum ym2149_volumeType {YM2149_VOLMANUAL, YM2149_VOLENVELOPE} ym2149_volumeType_t;
 typedef enum ym2149_envelopeShape {YM2149_ENV_0000, YM2149_ENV_0100, YM2149_ENV_1000, YM2149_ENV_1001, YM2149_ENV_1010, YM2149_ENV_1011, YM2149_ENV_1100, YM2149_ENV_1101, YM2149_ENV_1110, YM2149_ENV_1111} ym2149_envelopeShape_t;
+
+unsigned int device_slot;
 
 void setDeviceSlot(unsigned int slot);
 
